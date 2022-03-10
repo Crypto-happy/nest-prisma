62555b900a127b483862947de18ea4d92684b741
happy <websitedevelopercrypro@gmail.com>
2022-03-11 07:22:51 +0800

    initial

---
+++ src/auth/dto/signup.input.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { InputType, Field } from '@nestjs/graphql';
+
+@InputType()
+export class SignupInput {
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @Field({ nullable: true })
+  firstname?: string;
+
+  @Field({ nullable: true })
+  lastname?: string;
+  
+  @Field({ nullable: true })
+  phonenumber?: string;
+  
+  @Field({ nullable: true })
+  birthday?: Date;
+}
